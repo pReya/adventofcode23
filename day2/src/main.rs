2fd99bf6b7f12054fc90a349d3a5a44b3b4688e4
Moritz Stückler <moritz@bitbetter.de>
2023-12-02 13:02:32 +0000

    feat: day 2 part 1

---
+++ day2/src/main.rs
@@ -0,0 +1,71 @@
+use std::fs;
+
+fn main() {
+    let contents =
+        fs::read_to_string("src/input.txt").expect("Should have been able to read the file");
+
+    part1(&contents);
+}
+
+fn part1(input: &str) {
+    let lines: Vec<&str> = input.lines().collect();
+
+    // Prepare data structure with game data
+    let games: Vec<&str> = lines
+        .iter()
+        .map(|l| -> &str {
+            let line_parts: Vec<&str> = l.split(": ").collect();
+            &line_parts[1]
+        })
+        .collect();
+
+    let game_data: Vec<Vec<Vec<i32>>> = games
+        .iter()
+        .map(|game| -> Vec<Vec<i32>> {
+            game.split("; ")
+                .map(|round| -> Vec<i32> {
+                    let colors: Vec<Vec<&str>> = round
+                        .split(", ")
+                        .map(|color_and_value| -> Vec<&str> {
+                            color_and_value.split(" ").collect()
+                        })
+                        .collect();
+
+                    let mut r_g_b: Vec<i32> = vec![0, 0, 0];
+
+                    for color in colors {
+                        if color[1] == "red" {
+                            r_g_b[0] = color[0].parse::<i32>().unwrap()
+                        } else if color[1] == "green" {
+                            r_g_b[1] = color[0].parse::<i32>().unwrap()
+                        } else if color[1] == "blue" {
+                            r_g_b[2] = color[0].parse::<i32>().unwrap()
+                        }
+                    }
+
+                    r_g_b
+                })
+                .collect()
+        })
+        .collect();
+
+    // Calculate solution
+    // [red, green, blue]
+    let bag = vec![12, 13, 14];
+    let mut sum: i32 = 0;
+
+    for (i, game) in game_data.iter().enumerate() {
+        let mut game_impossible = false;
+        for showing in game {
+            if !((showing[0] <= bag[0]) && (showing[1] <= bag[1]) && (showing[2] <= bag[2])) {
+                println!("Game {} is impossible. {:?}", i as i32 + 1, showing);
+                game_impossible = true;
+                break;
+            }
+        }
+        if game_impossible != true {
+            sum = sum + (i as i32 + 1)
+        }
+    }
+    println!("Sum Part 1: {}", sum);
+}
